42051d3c914a9f617c5c47ef11159e95d204c240
manu300668 <eumanu@msn.com>
2026-01-21 13:21:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulario preparado. Más adelante se conectará EmailJS.');
+});
