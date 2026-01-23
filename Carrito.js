b588d87f697d9d2386e0a7c76b83ffb7d92bdc38
manu300668 <eumanu@msn.com>
2026-01-23 19:38:50 +0100

    Update Carrito.js

--- Carrito.js
+++ Carrito.js
@@ -1,1 +1,11 @@
+let carrito = [];
 
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    carrito.push({
+      producto: 'Sudadera Oversize',
+      precio: 65
+    });
+    alert('Producto añadido al carrito');
+  });
+});
