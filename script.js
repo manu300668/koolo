a060d917a64f95468266bb1bff2bd5814be5416d
manu300668 <eumanu@msn.com>
2026-01-24 16:30:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// =========================
+// SCROLL REVEAL AGRESIVO
+// =========================
 
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const windowHeight = window.innerHeight;
+  const revealPoint = 100;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
